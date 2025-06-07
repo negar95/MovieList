5a92def7d7dcfffcb99b7b0f002a47752069508c
Negar M <negar.moshtaghi@gmail.com>
2025-06-07 20:21:26 +0330

    improve About page

--- MovieList/MovieList/View/AboutView.swift
+++ MovieList/MovieList/View/AboutView.swift
@@ -8,18 +8,51 @@
 import SwiftUI
 
 struct AboutView: View {
+    private let appVersion: String = {
+        Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "1.0"
+    }()
+
     var body: some View {
-        VStack {
-            Text("Movie List")
-                .font(.title)
-                .foregroundStyle(.primary)
-            Text("This is a movie list app, using \"themoviedb.org\". You can search movies and tap on them to read overview.")
-                .font(.caption)
-                .foregroundStyle(.primary)
+        VStack(alignment: .center, spacing: 20) {
+
+            Image(systemName: "info.circle")
+                .resizable()
+                .frame(width: 50, height: 50)
+                .foregroundStyle(.secondary)
+                .padding(.bottom)
+                .padding(.bottom)
+            HStack {
+                Text("Movie List")
+                    .font(.title3)
+                    .bold()
+                    .foregroundStyle(.primary)
+
+                Text("(v\(appVersion))")
+                    .font(.subheadline)
+                    .bold()
+                    .foregroundStyle(.secondary)
+                Spacer()
+            }
+            .padding(.horizontal)
+
+            Text("This app uses \"themoviedb.org\" to let you search for movies and view their overviews.")
+                .font(.body)
+                .foregroundStyle(.secondary)
+                .multilineTextAlignment(.leading)
+                .padding(.horizontal)
+
+            Spacer()
+            Text("by  [Negar Moshtaghi](negarmoshtaghi@gmail.com)")
+                .font(.body)
+                .foregroundStyle(.secondary)
         }
+        .padding()
+        .navigationTitle("About")
     }
 }
 
 #Preview {
-    AboutView()
+    NavigationView {
+        AboutView()
+    }
 }
